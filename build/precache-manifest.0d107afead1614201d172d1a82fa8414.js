b139c0554b1b46d7faaba1b694433e4ba9ad0b04
Ed Ringger <edringger@gmail.com>
2020-08-10 11:28:06 -0700

    pushing updates from build

---
+++ build/precache-manifest.0d107afead1614201d172d1a82fa8414.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "842b6025bafabf9cfeb672676d4a4ba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11b0f4f9d8bc24bfb823",
+    "url": "/static/css/main.5903add9.chunk.css"
+  },
+  {
+    "revision": "2f4f1aa77e91007c2714",
+    "url": "/static/js/2.834d18ec.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.834d18ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b0f4f9d8bc24bfb823",
+    "url": "/static/js/main.1906e26c.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  },
+  {
+    "revision": "b628022f1f8cb8c93847ff49c28eace7",
+    "url": "/static/media/vbb-full-logo.b628022f.png"
+  },
+  {
+    "revision": "1a7d03240cf0aa027bf15e7ebecffeef",
+    "url": "/static/media/vbb-picture-logo.1a7d0324.png"
+  },
+  {
+    "revision": "c676dc5c992a0883153cbcc5709ea8cc",
+    "url": "/static/media/vbb_in_action.c676dc5c.png"
+  }
+]);
