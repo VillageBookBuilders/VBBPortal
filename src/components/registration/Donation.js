8ed1c9daf8c4704cc80b143fbe4efc23bab7a599
ferror18 <ferror18@gmail.com>
2021-02-05 14:58:26 -0800

    Started replicating donation component in reg form

--- src/components/registration/Donation.js
+++ src/components/registration/Donation.js
@@ -3,14 +3,20 @@ import { Elements } from '@stripe/react-stripe-js';
 import CheckoutForm from './CheckoutForm';
 import { loadStripe } from '@stripe/stripe-js';
 
+
 const stripePromise = loadStripe('pk_live_nKWeX2hm4oX0Nz3JqpZyyvix');
 
 function Donation() {
   return(
-    <div>
-      <Elements stripe={stripePromise}>
-        <CheckoutForm/> 
-      </Elements>
+    <div className='donation-form-container'>
+      <div className='donation-form'>
+        <div className='freq-buttons'>
+          <div className='freqbtn'><h5>MONTHLY</h5></div>
+          <div className='freqbtn'><h5>GIVE ONCE</h5></div>
+        </div>
+        <div className='amount-options'></div>
+        <div className='custom-and-submit'></div>
+      </div>
     </div>
   );
 }
