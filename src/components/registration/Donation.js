d76eda53afc5caa3309703b5f71ddac03d30d5fd
ferror18 <ferror18@gmail.com>
2021-02-07 10:55:42 -0800

    Removed react-stripejs

--- src/components/registration/Donation.js
+++ src/components/registration/Donation.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import {CardElement, Elements, PaymentRequestButtonElement} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
 const stripePromise = loadStripe('pk_test_8EURQq6ARdRXnNH6AiQh0gXQ');
 
@@ -17,33 +16,38 @@ function Donation() {
       mode: 'payment',
       successUrl: 'http://localhost:3000/register',
       cancelUrl: 'http://localhost:3000/register',
-    });
+    })
     // If `redirectToCheckout` fails due to a browser or network
     // error, display the localized error message to your customer
     // using `error.message`.
   };
   return(
     <>
-    <Elements stripe={stripePromise}>
     <div className='donation-form-container'>
       <div className='donation-form'>
         <div className='freq-buttons'>
-          <div onClick={()=>setFreq(true)} className={freq?'freqbtn selected':'freqbtn'}><h5>MONTHLY</h5></div>
-          <div onClick={()=>setFreq(false)} className={!freq?'freqbtn selected':'freqbtn'}><h5>GIVE ONCE</h5></div>
+          <div onClick={()=>setFreq(true)} className={freq?'freqbtn freqSelected':'freqbtn'}><h5>MONTHLY</h5></div>
+          <div onClick={()=>setFreq(false)} className={!freq?'freqbtn freqSelected':'freqbtn'}><h5>GIVE ONCE</h5></div>
         </div>
         <div className='amount-options'>
-          <div className='tier1'></div>
+          <div className='tier1 tierSekected'></div>
           <div className='tier2'></div>
           <div className='tier3'></div>
         </div>
         <div className='custom-and-submit'>
-            <button role="link" onClick={handleCheckOutDonations}>
-                  Check out dontaion prices
-                </button>
+          <div className='custom-and-submit-content-container'>
+          <h1>Here's how you can support your mentee.
+            Your donation covers more than you think</h1>
+          <p>A monthly donation of $5 will allow your mentee to have regular access to a computer, 
+            headphones, Wi-Fi connection, a safe learning environment, 
+            and Khan Academy's award-winning educational programs.</p>
+            <div className='Donate Button' onClick={handleCheckOutDonations}>
+                  Donate!
+                </div>
+            </div>
         </div>
       </div>
     </div>
-    </Elements>
     </>
   );
 }
