1d9ae189d2985e8ce63168b28d4f7187f3dc970b
ferror18 <ferror18@gmail.com>
2021-02-17 20:52:00 -0800

    Added iframe to step4

--- src/components/registration/Step4.js
+++ src/components/registration/Step4.js
@@ -1,5 +1,6 @@
 import React from "react";
-import Donation from "../registration/Donation.js"
+import { Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 function Step4(props) {
   if (props.state.currentStep !== 4) {
     return null;
@@ -10,9 +11,19 @@ function Step4(props) {
       <h1>100% OF YOUR DONTAION</h1>
       <h4>GOES TO BRINGING HOPE THROUGH EDUCATION</h4>
       <p>All online donations cover the mentor program and other in-country projects like libraries, solar panels, bathrooms, books, and computers. </p>
-      </div>
-        <Donation/>
       
+      <iframe width="800px" height="450px" 
+      src="https://www.youtube.com/embed/saahzNO1-aU?controls=0&autoplay=1" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      
+      <NavLink className='donateButton btn' to="/donate/" 
+          onClick={(event) => {event.preventDefault(); window.open(window.location.pathname.replace('register','donate'));}}>
+          <h1>Donate</h1>
+      </NavLink>
+
+      
+      </div>
     </div>
   );
 }
