25bb7af5d0a6881176ea48213e92fecc5ac40d8a
ferror18 <ferror18@gmail.com>
2021-03-05 12:22:47 -0800

    .

--- src/components/registration/ThankYouPage.js
+++ src/components/registration/ThankYouPage.js
@@ -11,11 +11,11 @@ export default function ThankYouPage() {
                 </div>
                 <div className='tpContentCardRigth'>
                 <h1>Thank you so much for donating!....</h1>
-                <h2>And, of course, thank you for choosing to be a mentor!</h2>
+                <h2>And, thank you for choosing to be a mentor!</h2>
                 <h6>
                 Our mission is to bring books, computers, and reliable internet to children living in rural undeserved communities all over the world, and we can't achieve that vision without you.
                 </h6>
-                <p>You can close this window now</p>
+                <br/><br/><p>You can close this window now</p>
                 </div>
             </div>
         </div>
