97b5c455f8d1e67200d39fc55e297e62b5b44848
Varun Raja <varunvraja@gmail.com>
2021-04-15 14:34:46 -0400

    change in django admin

--- api/admin.py
+++ api/admin.py
@@ -48,6 +48,8 @@ class MentorProfileAdmin(admin.ModelAdmin):
         "time_zone",
         "personal_email",
         "vbb_email",
+        "isVerified"
+        "isVerified_notes"
         "phone",
         "languages",
         "adult",
