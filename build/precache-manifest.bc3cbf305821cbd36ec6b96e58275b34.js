e6ce4cbd67d7be8c508d514767e27c0070a53a90
edringger <edringger@gmail.com>
2021-05-06 00:40:20 -0600

    precache changes??

---
+++ build/precache-manifest.bc3cbf305821cbd36ec6b96e58275b34.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c209f04b6bb60705e6b46ceaf52af0fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29b5a4949cc750e1e3c1",
+    "url": "/static/css/main.620f14b0.chunk.css"
+  },
+  {
+    "revision": "75b6a98f2658256c5c98",
+    "url": "/static/js/2.d4c2608e.chunk.js"
+  },
+  {
+    "revision": "1ad0b9f22a131161aef6661e04cbdf56",
+    "url": "/static/js/2.d4c2608e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b5a4949cc750e1e3c1",
+    "url": "/static/js/main.f8aadf4a.chunk.js"
+  },
+  {
+    "revision": "b757cbfa5b52942fc711",
+    "url": "/static/js/runtime-main.e327d292.js"
+  },
+  {
+    "revision": "5789b31aea304eeba8ee5ad1ce53c290",
+    "url": "/static/media/circle.5789b31a.svg"
+  },
+  {
+    "revision": "7a38a3813fb46303f1ce3d15513cac31",
+    "url": "/static/media/tick.7a38a381.svg"
+  },
+  {
+    "revision": "b628022f1f8cb8c93847ff49c28eace7",
+    "url": "/static/media/vbb-full-logo.b628022f.png"
+  },
+  {
+    "revision": "c676dc5c992a0883153cbcc5709ea8cc",
+    "url": "/static/media/vbb-in-action.c676dc5c.png"
+  },
+  {
+    "revision": "7ad1e10ca052913f598ecb1acdbd65f5",
+    "url": "/static/media/vbb-mentee-computer.7ad1e10c.png"
+  },
+  {
+    "revision": "1a7d03240cf0aa027bf15e7ebecffeef",
+    "url": "/static/media/vbb-picture-logo.1a7d0324.png"
+  }
+]);
