21c27116617313f51f972bdc3e26a93066f24c67
edringger <edringger@gmail.com>
2021-05-18 11:39:52 -0600

    removed session authentication line

--- vbbportal/settings.py
+++ vbbportal/settings.py
@@ -94,11 +94,8 @@ SITE_ID = 1
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework.authentication.TokenAuthentication",
-        'rest_framework.authentication.SessionAuthentication',
-
-
+        # 'rest_framework.authentication.SessionAuthentication',
     ),
-
 }
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
