fb96539359f7ccd1bf38fcaa397e8bbcf823919b
edringger <edringger@gmail.com>
2021-05-18 12:26:32 -0600

    removed is_verified_notes

--- api/models.py
+++ api/models.py
@@ -52,7 +52,6 @@ class MentorProfile(models.Model):
     city = models.CharField(max_length=70, null=True, blank=True)
     advisor_notes = models.TextField(max_length=512,null=True,blank=True)
     isVerified = models.BooleanField(default=False, null=False)
-    isVerified_notes = models.TextField(max_length=512,null=True,blank=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
